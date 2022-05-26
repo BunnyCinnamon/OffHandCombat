c059fafcc3ab75761e58d4a934abc30a46707dae
ArekkuusuJerii <edgargcx99@gmail.com>
2022-05-26 14:56:51 -0500

    ifghwGHFUWFHWHFW

--- common/src/main/java/cinnamon/ofc/mixin/GameModeAttackMixin.java
+++ common/src/main/java/cinnamon/ofc/mixin/GameModeAttackMixin.java
@@ -6,6 +6,7 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.multiplayer.MultiPlayerGameMode;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -18,7 +19,14 @@ public class GameModeAttackMixin {
 
     @Inject(method = "attack", at = @At(target = "Lnet/minecraft/world/entity/player/Player;attack(Lnet/minecraft/world/entity/Entity;)V", value = "INVOKE"), cancellable = true)
     public void attack(Player player, Entity entity, CallbackInfo ci) {
-        if(Mod.get(player).doOverride) {
+        Mod.Data data = Mod.get(player);
+        if(data.attackStrengthTicker <= 5 || player.attackStrengthTicker <= 5) {
+            entity.invulnerableTime = 0;
+            if(entity instanceof LivingEntity) {
+                ((LivingEntity) entity).lastHurt = 0;
+            }
+        }
+        if(data.doOverride) {
             HandPlatform.attack(player, entity);
             ci.cancel();
         }
--- common/src/main/java/cinnamon/ofc/mixin/ListenerImplMixin.java
+++ common/src/main/java/cinnamon/ofc/mixin/ListenerImplMixin.java
@@ -5,6 +5,8 @@ import cinnamon.ofc.Mod;
 import cinnamon.ofc.Listener;
 import net.minecraft.network.protocol.game.ServerboundInteractPacket;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Dynamic;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -24,7 +26,17 @@ public class ListenerImplMixin implements Listener {
     @Inject(method = "onAttack()V", at = @At(target = "Lnet/minecraft/server/level/ServerPlayer;attack(Lnet/minecraft/world/entity/Entity;)V", value = "INVOKE", shift = At.Shift.BEFORE))
     public void onAttack(CallbackInfo ci) {
         if (((Packet) this.serverboundInteractPacket).get()) {
-            Mod.get(this.player).doOverride = true;
+            Mod.Data data = Mod.get(this.player);
+            if(data.attackStrengthTicker <= 5 || player.attackStrengthTicker <= 5) {
+                Entity target = this.serverboundInteractPacket.getTarget(this.player.getLevel());
+                if (target != null) {
+                    target.invulnerableTime = 0;
+                    if(target instanceof LivingEntity) {
+                        ((LivingEntity) target).lastHurt = 0;
+                    }
+                }
+            }
+            data.doOverride = true;
             this.player = null;
             this.serverboundInteractPacket = null;
         }
